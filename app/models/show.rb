9e6e88566e8cde150c78b94fbca6dac6d2d89081
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-13 22:16:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where(self.highest_rating)
+  show =  Show.where(self.highest_rating)
     # (highest_rating)
   end
 
